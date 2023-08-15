23708d7087753f82aa904e4e63683ce813f9ca38
Kun <1amb4a@gmail.com>
2023-08-15 20:25:39 +0900

    fix: Update dotfiles

---
+++ OSX/init.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions                                                                                                                            │
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting;
+git clone https://github.com/z-shell/F-Sy-H.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/F-Sy-H
+
+npm install -g https://github.com/banga/git-split-diffs
